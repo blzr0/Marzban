afaa18c9b7d1eccc1bd76e951efb212480efc9b1
Vadim <yakana@mail.ru>
2026-08-25 21:49:45 +0000

    chore: bump panel version to 0.8.27

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from fastapi.routing import APIRoute
 
 from config import ALLOWED_ORIGINS, DOCS, XRAY_SUBSCRIPTION_PATH
 
-__version__ = "0.8.26"
+__version__ = "0.8.27"
 
 app = FastAPI(
     title="MarzbanAPI",
